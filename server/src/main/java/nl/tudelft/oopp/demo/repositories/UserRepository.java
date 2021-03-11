01142aa41d515e2835d45e09c92d896826744962
Athanasios Christopoulos <A.Christopoulos@student.tudelft.nl>
2021-03-11 10:41:21 +0100

    Added UserRepository in repositories

---
+++ server/src/main/java/nl/tudelft/oopp/demo/repositories/UserRepository.java
@@ -0,0 +1,7 @@
+package nl.tudelft.oopp.demo.repositories;
+
+import nl.tudelft.oopp.demo.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, String> {}
+
