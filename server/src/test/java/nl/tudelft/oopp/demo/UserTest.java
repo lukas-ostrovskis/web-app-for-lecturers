f3620dfd0fff583bbe9ec18ed88bc97a7f233621
Athanasios Christopoulos <A.Christopoulos@student.tudelft.nl>
2021-03-11 10:31:07 +0100

    Corrected some code duplication in the test class for user

--- server/src/test/java/nl/tudelft/oopp/demo/UserTest.java
+++ server/src/test/java/nl/tudelft/oopp/demo/UserTest.java
@@ -1,86 +1,58 @@
 package nl.tudelft.oopp.demo;
 
 import nl.tudelft.oopp.demo.entities.User;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class UserTest {
 
+    private static User user;
+    private static User user1;
+    private static String id;
+    private static String name;
+    private static String email;
+    private static String role;
+    private static String ip;
+
+    @BeforeAll
+    static void init() {
+        id = "1234AB";
+        name = "John Doe";
+        email = "abc@gmail.com";
+        role = "lecturer";
+        ip = "123.456.678";
+        user = new User(id, name, email, role, ip);
+        user1 = new User(id, name, email, role, ip);
+    }
+
+
     @Test
     public void constructorTest(){
-        String id = "1234AB";
-        String name = "John Doe";
-        String email = "abc@gmail.com";
-        String role = "lecturer";
-        String ip = "123.456.678";
-        User user = new User(id,name,email,role,ip);
         assertNotNull(user);
     }
     @Test
     public void getIDTest(){
-        String id = "1234AB";
-        String name = "John Doe";
-        String email = "abc@gmail.com";
-        String role = "lecturer";
-        String ip = "123.456.678";
-        User user = new User(id,name,email,role,ip);
         assertEquals("1234AB", user.getId());
     }
     @Test
     public void getNameTest(){
-        String id = "1234AB";
-        String name = "John Doe";
-        String email = "abc@gmail.com";
-        String role = "lecturer";
-        String ip = "123.456.678";
-        User user = new User(id,name,email,role,ip);
         assertEquals("John Doe", user.getName());
     }
     @Test
     public void getEmailTest(){
-        String id = "1234AB";
-        String name = "John Doe";
-        String email = "abc@gmail.com";
-        String role = "lecturer";
-        String ip = "123.456.678";
-        User user = new User(id,name,email,role,ip);
         assertEquals("abc@gmail.com", user.getEmail());
     }
     @Test
     public void getRoleTest(){
-        String id = "1234AB";
-        String name = "John Doe";
-        String email = "abc@gmail.com";
-        String role = "lecturer";
-        String ip = "123.456.678";
-        User user = new User(id,name,email,role,ip);
         assertEquals("lecturer", user.getRole());
     }
     @Test
     public void getIPTest(){
-        String id = "1234AB";
-        String name = "John Doe";
-        String email = "abc@gmail.com";
-        String role = "lecturer";
-        String ip = "123.456.678";
-        User user = new User(id,name,email,role,ip);
         assertEquals("123.456.678", user.getIp());
     }
 
     @Test
     public void equalsTest(){
-        String id = "1234AB";
-        String name = "John Doe";
-        String email = "abc@gmail.com";
-        String role = "lecturer";
-        String ip = "123.456.678";
-        User user = new User(id,name,email,role,ip);
-        String id1 = "1234AB";
-        String name1 = "John Doe";
-        String email1 = "abc@gmail.com";
-        String role1 = "lecturer";
-        String ip1 = "123.456.678";
-        User user1 = new User(id,name,email,role,ip);
         assertTrue(user.equals(user1));
     }
 
