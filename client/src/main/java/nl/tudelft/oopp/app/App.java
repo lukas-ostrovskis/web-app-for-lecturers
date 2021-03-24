39275d2a1bb6bf9c1e81d0f661531e774e0a6e90
David Kochergin <d.v.kochergin@student.tudelft.nl>
2021-03-24 12:31:24 +0100

    Added menu controller

---
+++ client/src/main/java/nl/tudelft/oopp/app/App.java
@@ -0,0 +1,17 @@
+package nl.tudelft.oopp.app;
+
+import nl.tudelft.oopp.app.views.MainView;
+
+/**
+ * Launches the main JavaFX application
+ *
+ * A workaround to avoid the gradle-related error:
+ *      Error: JavaFX runtime components are missing, and are required to run this application
+ *
+ * Source: https://github.com/openjfx/javafx-gradle-plugin/issues/31
+ */
+public class App {
+    public static void main(String[] args) {
+        MainView.main(new String[0]);
+    }
+}
