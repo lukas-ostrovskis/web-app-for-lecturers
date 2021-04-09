6830ec06b34c6d49c33c0cf409aa4debecf40ff0
Petr Persianov <p.persianov@student.tudelft.nl>
2021-04-09 17:51:56 +0200

    fixed style warning in test files on the client side

--- client/src/test/java/nl/tudelft/oopp/app/data/UserTest.java
+++ client/src/test/java/nl/tudelft/oopp/app/data/UserTest.java
@@ -4,7 +4,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
